045a50eb0370af7f7b4089745920a6e09de46ebe
csg <jumperbotyt@gmail.com>
2022-07-11 07:54:30 +0800

    :sparkles: Split/Lap Function

--- src/SomeUtils/Clock/Stopwatch.java
+++ src/SomeUtils/Clock/Stopwatch.java
@@ -1,19 +1,29 @@
 package SomeUtils.Clock;
 
+import java.util.ArrayList;
+
 public class Stopwatch{
   //For demo only!
   public static void main(String[]a){
-    Stopwatch stopwatch=new Stopwatch();
+    final Stopwatch stopwatch=new Stopwatch();
     stopwatch.start();
     try{
       java.lang.Thread.sleep(1000);
     }catch(Exception e){}
+    stopwatch.split();
     System.out.println("Time elapsed in nanoseconds: "+stopwatch.getTimeElapsed());
     System.out.println("Time elapsed in milliseconds: "+stopwatch.getTimeElapsed(Stopwatch.millis));
     System.out.println("Time elapsed in seconds: "+stopwatch.getTimeElapsed(Stopwatch.seconds));
+    try{
+      java.lang.Thread.sleep(1000);
+    }catch(Exception e){}
+    stopwatch.split();
+    final long[][] laps=stopwatch.getLaps();
+    for(long[] lap:laps){
+      System.out.println(java.util.Arrays.toString(lap));
+    }
   }
   //The actual class!
-  //Stopwatch
   long start=System.nanoTime();
   public final static int millis=0;
   public final static int seconds=1;
@@ -22,7 +32,7 @@ public class Stopwatch{
     start=System.nanoTime();
   }
   public long getTimeElapsed(){
-    return System.nanoTime()-start;
+    return getTimeElapsed(nanos);
   }
   public long getTimeElapsed(final int unit){
     final long timeElapsed=System.nanoTime()-start;
@@ -31,5 +41,30 @@ public class Stopwatch{
      (unit==seconds)?(timeElapsed/1000000000):
       timeElapsed
     );
+  }
+  public void lap(){
+    split();
+  }
+  final ArrayList<long[]> laps=new ArrayList<>();
+  long lastSplit=-1;
+  public void split(){
+    final long elapsed=getTimeElapsed();
+    if(lastSplit==-1)
+      lastSplit=elapsed;
+    else
+      lastSplit=elapsed-lastSplit;
+    laps.add(new long[]{lastSplit, elapsed});
+    lastSplit=elapsed;
+  }
+  public long[][] getLaps(){
+    final int lapSize=laps.size();
+    final long[][] lapsArr=new long[lapSize][3];
+    for(int i=0;i<lapSize;i++){
+      final long[] curLap=laps.get(i);
+      lapsArr[i][0]=i;
+      lapsArr[i][1]=curLap[0];
+      lapsArr[i][2]=curLap[1];
+    }
+    return lapsArr;
   }
 }
